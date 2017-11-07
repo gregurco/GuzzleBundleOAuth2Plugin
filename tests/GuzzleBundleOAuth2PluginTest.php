e1f474a07b2cd57defe042d5e3f57cee161df1e4
Adrien Roches <rochesadrien@gmail.com>
2017-11-07 13:03:42 +0100

    [FEATURE] Add parameter config for

--- tests/GuzzleBundleOAuth2PluginTest.php
+++ tests/GuzzleBundleOAuth2PluginTest.php
@@ -90,6 +90,7 @@ class GuzzleBundleOAuth2PluginTest extends TestCase
                 'client_id' => 'test-client-id',
                 'client_secret' => '',
                 'scope' => 'administration',
+                'resource' => null,
                 'auth_location' => 'headers',
                 'grant_type' => PasswordCredentials::class,
             ],
