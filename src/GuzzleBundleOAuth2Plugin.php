d28f9db58af093246696c6cce0b266ccfeaf0eb1
Adrien Roches <rochesadrien@gmail.com>
2017-11-07 11:51:53 +0100

    [UPDATE] Add auth_location config

--- src/GuzzleBundleOAuth2Plugin.php
+++ src/GuzzleBundleOAuth2Plugin.php
@@ -42,6 +42,7 @@ class GuzzleBundleOAuth2Plugin extends Bundle implements EightPointsGuzzleBundle
                 PasswordCredentials::CONFIG_CLIENT_ID => $config['client_id'],
                 PasswordCredentials::CONFIG_CLIENT_SECRET => $config['client_secret'],
                 PasswordCredentials::CONFIG_TOKEN_URL => $config['token_url'],
+                PasswordCredentials::CONFIG_AUTH_LOCATION => $config['auth_location'],
                 'scope' => $config['scope'],
             ];
 
@@ -98,6 +99,7 @@ class GuzzleBundleOAuth2Plugin extends Bundle implements EightPointsGuzzleBundle
                 ->scalarNode('client_secret')->defaultNull()->end()
                 ->scalarNode('token_url')->defaultNull()->end()
                 ->scalarNode('scope')->defaultNull()->end()
+                ->scalarNode('auth_location')->defaultValue('headers')->end()
                 ->scalarNode('grant_type')->defaultValue(PasswordCredentials::class)->end()
             ->end();
     }
